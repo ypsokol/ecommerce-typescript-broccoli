bf4d885a66ebc1c809601f5f63c994951e1f0e3c
ypsokol <madnseries@gmail.com>
2022-05-12 18:08:45 +0300

    add: disable typescript during build

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     domains: ["cdn.sanity.ts.io", "cdn.sanity.io"],
   },
